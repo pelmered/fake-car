272829e4995a375fed91a5463de17fe8c4ff970e
Peter Elmered <peter@elmered.com>
2018-05-22 22:21:15 +0200

    Add tests

---
+++ tests/Faker/FakecarTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Faker\Tests\Provider;
+
+use Faker\Factory;
+use Faker\Generator;
+use Faker\Provider\Fakecar;
+use PHPUnit\Framework\TestCase;
+
+class FakecarTest extends TestCase
+{
+    /**
+     * @var Generator
+     */
+    private $faker;
+
+    public function setUp()
+    {
+        $faker = Factory::create();
+        $faker->addProvider(new Fakecar($faker));
+        $this->faker = $faker;
+    }
+
+    public function getProtectedProperty( $property, $class = null )
+    {
+        if( is_null($class))
+        {
+            $class = new Fakecar($this->faker);
+        }
+
+        $reflection = new \ReflectionClass($class);
+        $reflection_property = $reflection->getProperty($property);
+        $reflection_property->setAccessible(true);
+
+        return $reflection_property->getValue($class, $property);
+    }
+
+    public function testVehicle()
+    {
+        $this->faker->seed(random_int(1, 9999));
+
+        $vehicleBrand = $this->faker->vehicleBrand();
+
+        $vehicleText = $this->faker->vehicle();
+        $brands = $this->getProtectedProperty('brandsWithModels');
+
+        foreach($brands as $brand => $models)
+        {
+            if(substr($vehicleText, 0, strlen($brand)) === $brand) {
+                foreach ($models as $model)
+                {
+                    if(substr($vehicleText,  -strlen($model)) === $model) {
+
+                        $this->assertStringEndsWith($model, $vehicleText);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public function testVehicleArray()
+    {
+        $vehicleArray = $this->faker->vehicleArray();
+
+        $this->assertArrayHasKey('brand', $vehicleArray);
+        $this->assertArrayHasKey('model', $vehicleArray);
+
+        $brandsArray = $this->getProtectedProperty('brandsWithModels');
+
+        $this->assertTrue(
+            in_array(
+                $vehicleArray['model'],
+                $brandsArray[$vehicleArray['brand']]
+            )
+        );
+
+    }
+
+    public function testVehicleBrand()
+    {
+        $this->assertTrue(
+            in_array(
+                $this->faker->vehicleBrand,
+                array_keys($this->getProtectedProperty('vehicleTypes')
+                )
+            )
+        );
+    }
+
+    public function testVehicleModel($make = null)
+    {
+        $this->faker->seed(random_int(1, 9999));
+
+        $vehicleBrand = $this->faker->vehicleBrand();
+
+        $modelArray = $this->getProtectedProperty('brandsWithModels');
+        $modelArray = $modelArray[$this->faker->vehicleBrand()];
+
+        $vehicleBrand = $this->faker->vehicleBrand();
+
+        $this->assertTrue(
+            in_array(
+                $this->faker->vehicleModel($vehicleBrand),
+                $this->getProtectedProperty('brandsWithModels')[$vehicleBrand]
+            )
+        );
+    }
+
+    public function testVehicleRegistration()
+    {
+        $this->assertRegExp('/[A-Z]{3}-[0-9]{3}/', $this->faker->vehicleRegistration());
+        $this->assertRegExp('/[A-Z]{2}-[0-9]{5}/', $this->faker->vehicleRegistration('[A-Z]{2}-[0-9]{5}'));
+    }
+
+    public function testVehicleType()
+    {
+        $this->assertTrue(in_array($this->faker->vehicleType, $this->getProtectedProperty('vehicleTypes')));
+    }
+
+    public function testVehicleFuelType()
+    {
+        $this->assertTrue(in_array($this->faker->vehicleFuelType, $this->getProtectedProperty('vehicleFuelTypes')));
+    }
+}
