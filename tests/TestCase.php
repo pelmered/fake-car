1120aede42affb7eec132cfa2e3f1f0122e3bb43
Peter Elmered <peter@elmered.com>
2024-09-26 00:44:16 +0200

    Rewrite test suite to Pest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests;
+namespace FakeCar\Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
--- tests/Unit/FakeCarTest.php
+++ tests/Unit/FakeCarTest.php
@@ -76,7 +76,7 @@ test('vehicle array', function () {
 });
 
 test('vehicle brand', function () {
-    expect((new FakeCarDataProvider)->getBrandsWithModels())->toHaveKey($this->faker->vehicleBrand);
+    expect((new FakeCarDataProvider)->getBrandsWithModels())->toHaveKey($this->faker->vehicleBrand());
 });
 
 test('vehicle model', function () {
@@ -93,35 +93,49 @@ test('vehicle registration', function () {
 });
 
 test('vehicle type', function () {
-    expect(FakeCarData::$vehicleTypes)->toContain($this->faker->vehicleType);
+    expect(FakeCarData::$vehicleTypes)->toContain($this->faker->vehicleType());
 });
 
 test('vehicle fuel type', function () {
-    expect(array_keys(FakeCarData::$vehicleFuelTypes))->toContain($this->faker->vehicleFuelType);
+    expect(array_keys(FakeCarData::$vehicleFuelTypes))->toContain($this->faker->vehicleFuelType());
 });
 
 test('vehicle door count', function () {
     for ($i = 0; $i<10; $i++) {
+
+        expect($this->faker->vehicleSeatCount())->toBeGreaterThanOrEqual(1)
+            ->and($this->faker->vehicleSeatCount())->toBeLessThanOrEqual(9)
+            ->and($this->faker->vehicleSeatCount())->toBeInt();
+
+        /*
         expect($this->logicalAnd(
             $this->isType('int'),
             $this->greaterThanOrEqual(2),
             $this->lessThanOrEqual(6)
         ))->toMatchConstraint($this->faker->vehicleDoorCount);
+        */
     }
 });
 
 test('vehicle seat count', function () {
     for ($i = 0; $i<10; $i++) {
+
+        expect($this->faker->vehicleSeatCount())->toBeGreaterThanOrEqual(1)
+            ->and($this->faker->vehicleSeatCount())->toBeLessThanOrEqual(9)
+            ->and($this->faker->vehicleSeatCount())->toBeInt();
+
+        /*
         expect($this->logicalAnd(
             $this->isType('int'),
             $this->greaterThanOrEqual(1),
             $this->lessThanOrEqual(9)
-        ))->toMatchConstraint($this->faker->vehicleSeatCount);
+        ))->toBe($this->faker->vehicleSeatCount);
+        */
     }
 });
 
 test('vehicle properties', function () {
-    $properties = $this->faker->vehicleProperties;
+    $properties = $this->faker->vehicleProperties();
     expect($properties)->toBeArray();
 
     $properties = $this->faker->vehicleProperties(2);
@@ -139,10 +153,10 @@ test('vehicle properties', function () {
 });
 
 test('vehicle gear box', function () {
-    expect(array_keys(FakeCarData::$vehicleGearBoxType))->toContain($this->faker->vehicleGearBoxType);
+    expect(array_keys(FakeCarData::$vehicleGearBoxType))->toContain($this->faker->vehicleGearBoxType());
 });
 
-test('get random elements from array', function () {
+test( 'get random elements from array', function () {
     $data = [
         'value1',
         'value2',
@@ -156,17 +170,17 @@ test('get random elements from array', function () {
         'value10',
     ];
 
-    expect(FakeCarHelper::getRandomElementsFromArray($data, 1))->toHaveCount(1);
-    expect(FakeCarHelper::getRandomElementsFromArray($data, 3))->toHaveCount(3);
-    expect(FakeCarHelper::getRandomElementsFromArray($data, 6))->toHaveCount(6);
-    expect(FakeCarHelper::getRandomElementsFromArray($data, 10))->toHaveCount(10);
-    expect(FakeCarHelper::getRandomElementsFromArray($data, 0))->toEqual([]);
+    expect(FakeCarHelper::getRandomElementsFromArray($data, 1))->toHaveCount(1)
+        ->and(FakeCarHelper::getRandomElementsFromArray($data, 3))->toHaveCount(3)
+        ->and(FakeCarHelper::getRandomElementsFromArray($data, 6))->toHaveCount(6)
+        ->and(FakeCarHelper::getRandomElementsFromArray($data, 10))->toHaveCount(10)
+        ->and(FakeCarHelper::getRandomElementsFromArray($data, 0))->toEqual([]);
 
     for ($i = 0; $i<50; $i++) {
         $result6 = FakeCarHelper::getRandomElementsFromArray($data, null);
 
-        expect(count($result6))->toBeGreaterThanOrEqual(0);
-        expect(count($result6))->toBeLessThanOrEqual(10);
+        expect(count($result6))->toBeGreaterThanOrEqual(0)
+            ->and(count($result6))->toBeLessThanOrEqual(10);
 
         foreach ($result6 as $r) {
             expect($data)->toContain($r);
@@ -193,9 +207,9 @@ test('get weighted', function () {
             $result[FakeCarHelper::getWeighted($data)]++;
         }
 
-        expect($result['key1'])->toBeGreaterThan($result['key2']);
-        expect($result['key2'])->toBeGreaterThan($result['key3']);
-        expect($result['key1'])->toBeGreaterThan($result['key3']);
+        expect($result['key1'])->toBeGreaterThan($result['key2'])
+           ->and($result['key2'])->toBeGreaterThan($result['key3'])
+           ->and($result['key1'])->toBeGreaterThan($result['key3']);
     }
 
     expect(FakeCarHelper::getWeighted([]))->toEqual('');
@@ -203,24 +217,20 @@ test('get weighted', function () {
 
 test('valid vin', function () {
     //Too short
-    expect($this->faker->validateVin('z2j9hhgr8Ahl1e3g'))->toBeFalse();
-
-    //Too long
-    expect($this->faker->validateVin('az2j9hhgr8Ahl1e3gs'))->toBeFalse();
-
-    //Invalid check digit
-    expect($this->faker->validateVin('z2j9hhgr2Ahl1e3gs'))->toBeFalse();
-
-    //Invalid
-    expect($this->faker->validateVin('z2j9hhgr8Ahl1e3gd'))->toBeFalse();
-
-    // Valid VINs
-    expect($this->faker->validateVin('z2j9hhgr8Ahl1e3gs'))->toBeTrue();
-    expect($this->faker->validateVin('n7u30vns7Ajsrb1nc'))->toBeTrue();
-    expect($this->faker->validateVin('3julknxb0A06hj41x'))->toBeTrue();
-    expect($this->faker->validateVin('yj12c8z40Aca2x6p3'))->toBeTrue();
-    expect($this->faker->validateVin('y95wf7gm1A9g7pz5z'))->toBeTrue();
-    expect($this->faker->validateVin('355430557Azf4u0vr'))->toBeTrue();
+    expect($this->faker->validateVin('z2j9hhgr8Ahl1e3g'))->toBeFalse()
+        //Too long
+        ->and($this->faker->validateVin('az2j9hhgr8Ahl1e3gs'))->toBeFalse()
+        //Invalid check digit
+        ->and($this->faker->validateVin('z2j9hhgr2Ahl1e3gs'))->toBeFalse()
+        //Invalid
+        ->and($this->faker->validateVin('z2j9hhgr8Ahl1e3gd'))->toBeFalse()
+        // Valid VINs
+        ->and($this->faker->validateVin('z2j9hhgr8Ahl1e3gs'))->toBeTrue()
+        ->and($this->faker->validateVin('n7u30vns7Ajsrb1nc'))->toBeTrue()
+        ->and($this->faker->validateVin('3julknxb0A06hj41x'))->toBeTrue()
+        ->and($this->faker->validateVin('yj12c8z40Aca2x6p3'))->toBeTrue()
+        ->and($this->faker->validateVin('y95wf7gm1A9g7pz5z'))->toBeTrue()
+        ->and($this->faker->validateVin('355430557Azf4u0vr'))->toBeTrue();
 });
 
 test('vin returns valid vin', function () {
@@ -228,58 +238,58 @@ test('vin returns valid vin', function () {
     expect($this->faker->validateVin($vin))->toBeTrue();
 });
 test('model year', function () {
-    expect($this->faker->modelYear(1980))->toEqual('A');
-    expect($this->faker->modelYear(2000))->toEqual('Y');
-    expect($this->faker->modelYear(2017))->toEqual('H');
-    expect($this->faker->modelYear(2018))->toEqual('J');
-    expect($this->faker->modelYear(2019))->toEqual('K');
+    expect($this->faker->modelYear(1980))->toEqual('A')
+        ->and($this->faker->modelYear(2000))->toEqual('Y')
+        ->and($this->faker->modelYear(2017))->toEqual('H')
+        ->and($this->faker->modelYear(2018))->toEqual('J')
+        ->and($this->faker->modelYear(2019))->toEqual('K');
 });
 test('transliterate', function () {
-    expect(callProtectedMethod(['O'], 'transliterate', new FakeCar($this->faker)))->toEqual(0);
-    expect(callProtectedMethod(['A'], 'transliterate', new FakeCar($this->faker)))->toEqual(1);
-    expect(callProtectedMethod(['K'], 'transliterate', new FakeCar($this->faker)))->toEqual(2);
+    expect(callProtectedMethod(['O'], 'transliterate', new FakeCar($this->faker)))->toEqual(0)
+        ->and(callProtectedMethod(['A'], 'transliterate', new FakeCar($this->faker)))->toEqual(1)
+        ->and(callProtectedMethod(['K'], 'transliterate', new FakeCar($this->faker)))->toEqual(2);
 });
 
 test('check digit', function () {
-    expect(callProtectedMethod(['z2j9hhgr8Ahl1e3g'], 'checkDigit', new FakeCar($this->faker)))->toEqual('4');
-    expect(callProtectedMethod(['n7u30vns7Ajsrb1n'], 'checkDigit', new FakeCar($this->faker)))->toEqual('1');
-    expect(callProtectedMethod(['3julknxb0A06hj41'], 'checkDigit', new FakeCar($this->faker)))->toEqual('8');
+    expect(callProtectedMethod(['z2j9hhgr8Ahl1e3g'], 'checkDigit', new FakeCar($this->faker)))->toEqual('4')
+        ->and(callProtectedMethod(['n7u30vns7Ajsrb1n'], 'checkDigit', new FakeCar($this->faker)))->toEqual('1')
+        ->and(callProtectedMethod(['3julknxb0A06hj41'], 'checkDigit', new FakeCar($this->faker)))->toEqual('8');
 });
 
 test('vin', function () {
     $vin = $this->faker->vin();
-    expect($vin)->toMatch('/[a-zA-Z0-9]{17}/');
-    expect($this->faker->validateVin($vin))->toBeTrue();
+    expect($vin)->toMatch('/[a-zA-Z0-9]{17}/')
+        ->and($this->faker->validateVin($vin))->toBeTrue();
 });
 
 test('engine power', function () {
-    $power = $this->faker->vehicleEnginePower;
-    expect($power)->toMatch('/^\d+ hp$/');
-    expect((int)explode(' ', $power)[0])->toBeGreaterThanOrEqual(100);
-    expect((int)explode(' ', $power)[0])->toBeLessThanOrEqual(1500);
+    $power = $this->faker->vehicleEnginePower();
+    expect($power)->toMatch('/^\d+ hp$/')
+        ->and((int) explode(' ', $power)[0])->toBeGreaterThanOrEqual(100)
+        ->and((int) explode(' ', $power)[0])->toBeLessThanOrEqual(1500);
 });
 
 test('engine torque', function () {
-    $torque = $this->faker->vehicleEngineTorque;
-    expect($torque)->toMatch('/^\d+ nm$/');
-    expect((int)explode(' ', $torque)[0])->toBeGreaterThanOrEqual(100);
-    expect((int)explode(' ', $torque)[0])->toBeLessThanOrEqual(700);
+    $torque = $this->faker->vehicleEngineTorque();
+    expect($torque)->toMatch('/^\d+ nm$/')
+        ->and((int) explode(' ', $torque)[0])->toBeGreaterThanOrEqual(100)
+        ->and((int) explode(' ', $torque)[0])->toBeLessThanOrEqual(700);
 });
 
 test('get range', function () {
     for($x = 0; $x<100; $x++) {
         $range = FakeCarHelper::getRange([1, 100], 0);
-        expect($range)->toMatch('/^\d+$/');
-        expect((int)$range)->toBeGreaterThanOrEqual(1);
-        expect((int)$range)->toBeLessThanOrEqual(100);
+        expect($range)->toMatch('/^\d+$/')
+            ->and((int) $range)->toBeGreaterThanOrEqual(1)
+            ->and((int) $range)->toBeLessThanOrEqual(100);
     }
 
     for($x = 0; $x<100; $x++) {
         $range = FakeCarHelper::getRange([100, 150], 2);
 
-        expect($range)->toMatch('/^\d+\.\d+$/');
-        expect((int)$range)->toBeGreaterThanOrEqual(100);
-        expect((int)$range)->toBeLessThanOrEqual(150);
+        expect($range)->toMatch('/^\d+\.\d+$/')
+            ->and((int) $range)->toBeGreaterThanOrEqual(100)
+            ->and((int) $range)->toBeLessThanOrEqual(150);
     }
 });
 test('get range invalid', function () {
@@ -294,16 +304,16 @@ test('get range with unit', function () {
     for($x = 0; $x<100; $x++) {
         $range = FakeCarHelper::getRangeWithUnit([2065, 2450], 'l', 0);
 
-        expect($range)->toMatch('/^\d+ l$/');
-        expect((int)$range)->toBeGreaterThanOrEqual(2065);
-        expect((int)$range)->toBeLessThanOrEqual(2450);
+        expect($range)->toMatch('/^\d+ l$/')
+            ->and((int) $range)->toBeGreaterThanOrEqual(2065)
+            ->and((int) $range)->toBeLessThanOrEqual(2450);
     }
 
     for($x = 0; $x<100; $x++) {
         $range = FakeCarHelper::getRangeWithUnit([200, 250], 'hp', 2);
 
-        expect($range)->toMatch('/^\d+\.\d+ hp$/');
-        expect((int)$range)->toBeGreaterThanOrEqual(200);
-        expect((int)$range)->toBeLessThanOrEqual(250);
+        expect($range)->toMatch('/^\d+\.\d+ hp$/')
+            ->and((int) $range)->toBeGreaterThanOrEqual(200)
+            ->and((int) $range)->toBeLessThanOrEqual(250);
     }
 });
