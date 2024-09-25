ca35552263b4ebefeb53b421155764788d4d6cae
Peter Elmered <peter@elmered.com>
2024-09-26 00:44:16 +0200

    Rewrite test suite to Pest

--- tests/Unit/ArchTest.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-arch()->preset()->laravel();
-arch()->preset()->security()->ignoring('parse_str');;
-
-arch()->expect('dd')->not->toBeUsed();
---
+++ tests/Unit/FakeCarDataProviderTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use FakeCar\Tests\TestProviders\BMWFakeCarData;
+use FakeCar\Tests\TestProviders\FerrariEnzoTestProvider;
+
+test('custom provider data source', function () {
+    $BMWCarData = new BMWFakeCarData;
+    $fakeCarDataProvider = new \Faker\Provider\FakeCarDataProvider($BMWCarData);
+
+    $faker = (new \Faker\Factory())::create();
+    $fakeCar = new \Faker\Provider\FakeCar($faker);
+    $fakeCar->setDataProvider($fakeCarDataProvider);
+    $faker->addProvider($fakeCar);
+
+    for ($i = 0; $i <= 100; $i++) {
+        $data = $faker->vehicleArray();
+        expect($data['brand'])->toEqual('BMW')
+            ->and($faker->vehicleBrand())->toEqual('BMW')
+            ->and((BMWFakeCarData::$brandsWithModels)['BMW'])->toContain($faker->vehicleModel())
+            ->and(BMWFakeCarData::$vehicleTypes)->toContain($faker->vehicleType());
+
+        $regex = '/^(?<value>\d+) (?<unit>[a-zA-Z]+)$/';
+        expect($faker->vehicleEnginePower())->toMatch($regex);
+        preg_match($regex, $faker->vehicleEnginePower(), $matches);
+        expect($matches['value'])->toBeGreaterThanOrEqual(BMWFakeCarData::$vehicleEnginePower['range'][0])
+            ->and($matches['value'])->toBeLessThanOrEqual(BMWFakeCarData::$vehicleEnginePower['range'][1])
+            ->and($matches['unit'])->toEqual('hp')
+            ->and($faker->vehicleEngineTorque())->toMatch($regex);
+
+        preg_match($regex, $faker->vehicleEngineTorque(), $matches);
+        expect($matches['value'])->toBeGreaterThanOrEqual(BMWFakeCarData::$vehicleEngineTorque['range'][0])
+            ->and($matches['value'])->toBeLessThanOrEqual(BMWFakeCarData::$vehicleEngineTorque['range'][1])
+            ->and($matches['unit'])->toEqual('nm');
+    }
+});
+
+test('custom provider class', function () {
+    $fakeCarDataProvider = new FerrariEnzoTestProvider();
+
+    $faker = (new \Faker\Factory())::create();
+    $fakeCar = new \Faker\Provider\FakeCar($faker);
+    $fakeCar->setDataProvider($fakeCarDataProvider);
+    $faker->addProvider($fakeCar);
+
+    $vehicleArray = $faker->vehicleArray();
+    expect($vehicleArray['brand'])->toEqual('Ferrari')
+        ->and($vehicleArray['model'])->toEqual('Enzo')
+        ->and($faker->vehicle())->toEqual('Ferrari Enzo')
+        ->and($faker->vehicleBrand())->toEqual('Ferrari')
+        ->and($faker->vehicleModel())->toEqual('Enzo')
+        ->and($faker->vehicleType())->toEqual('coupe')
+        ->and($faker->vehicleFuelType())->toEqual('gasoline')
+        ->and($faker->vehicleDoorCount())->toEqual(2)
+        ->and($faker->vehicleSeatCount())->toEqual(2)
+        ->and($faker->vehicleProperties())->toEqual([
+            'Air condition',
+            'GPS',
+            'Leather seats',
+        ]);
+
+});
